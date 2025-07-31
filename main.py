3970b71278ad2edef43b0e35e4d26b7aa3748bb2
Xurshidbek003 <xurshidbekt571@gmail.com>
2025-07-31 10:52:07 +0500

    second

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, Query
 from sqlalchemy.orm import Session
 from fastapi.middleware.cors import CORSMiddleware
 from database import database
@@ -7,20 +7,24 @@ from datetime import datetime
 
 app = FastAPI()
 
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins = ["*"],
-    allow_methods = ["*"],
+    allow_methods = ["*"]
 )
 
 
 @app.get('/get')
-def elonlarni_korish(db: Session = Depends(database)):
-    return db.query(Elonlar).all()
+def elonlarni_korish(name: str = None, db: Session = Depends(database)):
+    if name:
+        return db.query(Elonlar).filter(Elonlar.name.like(f"%{name}%")).all()
+    else:
+        return db.query(Elonlar).all()
 
 
 @app.post('/add')
-def elon_qoshish(name: str, description: str, price: float, status: bool, created_at: datetime = datetime.now(), db: Session = Depends(database)):
+def elon_qoshish(description: str, status: bool, name: str = Query(min_length=3), price: float = Query(gt=1, lt=1000000), created_at: datetime = datetime.now(), db: Session = Depends(database)):
     elon = Elonlar(
         name = name,
         description = description,
